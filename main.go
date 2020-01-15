7fa32a776ec7d104a0a97b2fe38c5e9b3333d60c
dschartman <schartmand@gmail.com>
2020-01-14 21:29:21 -0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+var barbellWeight = 45.0
+
+func plateCalc(totalWeight float64) []float64 {
+	adjustedWeight := totalWeight - barbellWeight
+	if adjustedWeight < 0 {
+		return []float64{}
+	}
+
+	plates := []float64{}
+	remainingWeight := adjustedWeight / 2
+	for remainingWeight > 0 {
+		plate := getLargestAvailablePlate(remainingWeight)
+		plates = append(plates, plate)
+
+		remainingWeight = remainingWeight - plate
+	}
+
+	return plates
+}
+
+var availableWeights = []float64{45, 35, 25, 15, 10, 5, 2.5, 1.25}
+
+func getLargestAvailablePlate(target float64) float64 {
+	for _, weight := range availableWeights {
+		diff := target - weight
+		if diff > 0 || diff == 0 {
+			return weight
+		}
+	}
+
+	return 0
+}
